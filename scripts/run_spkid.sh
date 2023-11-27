3c45efdb652201b31dc825eff34f0becad3369a6
xavimorenoo <xavier.moreno@estudiantat.upc.edu>
2023-11-27 15:59:32 +0100

    Clase 27/11

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -115,7 +115,7 @@ for cmd in $*; do
        for dir in $db_devel/BLOCK*/SES* ; do
            name=${dir/*\/}
            echo $name ----
-           EXEC="gmm_train -v 1 -T 0.001 -N 5 -m 1 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train"
+           EXEC="gmm_train -v 1 -T 0.001 -N 5 -m 5 -d $w/$FEAT -e $FEAT -g $w/gmm/$FEAT/$name.gmm $lists/class/$name.train"
            echo $EXEC && $EXEC || exit 1
            echo
        done
--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -213,6 +213,10 @@ namespace upc
 			//
 			// Update old_prob, new_prob and inc_prob in order to stop the loop if logprob does not
 			// increase more than inc_threshold.
+
+			new_prob = em_expectation(data, weights);
+			em_maximization(data, weights);
+
 			if (verbose & 01)
 				cout << "GMM nmix=" << nmix << "\tite=" << iteration << "\tlog(prob)=" << new_prob << "\tinc=" << inc_prob << endl;
 		}
