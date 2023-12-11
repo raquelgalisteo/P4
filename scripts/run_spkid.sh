bd829b4efaf75cce2d97f9f8ef2413bcdcf0cd00
xavimorenoo <xavier.moreno@estudiantat.upc.edu>
2023-12-11 15:54:01 +0100

    Clase 11/12

--- scripts/run_spkid.sh
+++ scripts/run_spkid.sh
@@ -175,7 +175,9 @@ for cmd in $*; do
        #
        # El fichero con el resultado del reconocimiento debe llamarse $FINAL_CLASS, que deberá estar en el
        # directorio de la práctica (PAV/P4).
-       echo "To be implemented ..."
+       compute_$FEAT $db_test $lists/final/class.test
+       EXEC="gmm_classify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm $lists/gmm.list $lists/final/class.test"
+       echo $EXEC && $EXEC | tee $FINAL_CLASS || exit 1
    
    elif [[ $cmd == finalverif ]]; then
        ## @file
@@ -194,8 +196,15 @@ for cmd in $*; do
        # candidato para la señal a verificar. En $FINAL_VERIF se pide que la tercera columna sea 1,
        # si se considera al candidato legítimo, o 0, si se considera impostor. Las instrucciones para
        # realizar este cambio de formato están en el enunciado de la práctica.
-       echo "To be implemented ..."
-   
+       compute_$FEAT $db_test $lists/final/verif.test 
+       EXEC="gmm_verify -d $w/$FEAT -e $FEAT -D $w/gmm/$FEAT -E gmm -w $world lists/gmm.list lists/final/verif.test lists/final/verif.test.candidates"
+       echo $EXEC && $EXEC | tee $TEMP_VERIF || exit 1
+       perl -ane 'print "$F[0]\t$F[1]\t";
+        if ($F[2] > 1.2058751047664) {print "1\n"} 
+        else {print "0\n"}' $TEMP_VERIF | tee $FINAL_VERIF
+       # El valor 1.2058751047664 es el que se obtiene despues de hacer el verify y luego el veryfierr
+       # Es el umbral óptimo, cuando mejoremos el sistem habrá que volverlo ha hacer y el nuevo umbral óptimo que salga habrá que sustituirlo por este que tenemos
+
    # If the command is not recognize, check if it is the name
    # of a feature and a compute_$FEAT function exists.
    elif [[ "$(type -t compute_$cmd)" = function ]]; then
